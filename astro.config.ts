f0b07945b34127610538acc4a777881b4923dc4d
badsparky <92095206+badsparky@users.noreply.github.com>
2025-07-24 12:39:52 +0900

    fix: Update site URL in astro.config.ts and upgrade ESLint dependencies in package-lock.json

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  site: 'https://badsparky.github.io',
+  site: 'https://sagalab-omu.github.io',
   base: '/sagalab-omu.github.io',
 
   integrations: [
