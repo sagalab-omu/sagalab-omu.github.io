5af771f54b181ab7f34051b7cb31b2de82aaddd2
naogon <92095206+badsparky@users.noreply.github.com>
2025-07-24 12:47:57 +0900

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -24,7 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
   site: 'https://sagalab-omu.github.io',
-  base: '/sagalab-omu.github.io',
+  base: '/',
 
   integrations: [
     tailwind({
