cd5dfb4bd60c01dba51a21069e4db6898672a5df
badsparky <92095206+badsparky@users.noreply.github.com>
2025-07-24 13:00:44 +0900

    fix: Correct site and base URL configuration in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  site: 'https://sagalab-omu.github.io/',
+  base: '/',
   integrations: [
     tailwind({
       applyBaseStyles: false,
